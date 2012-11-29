9fbe345f0008b0c3de9df8e71bd1bee4fc902b50
peter <tasker82@gmail.com>
2012-11-28 21:58:21 -0500

    adding in pause button...not hooked up, but it's there!

--- src/com/example/petetimer/Second.java
+++ src/com/example/petetimer/Second.java
@@ -4,12 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.v4.app.NavUtils;
-import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import com.example.petetimer.CountDownTimerTwo;
 
 public class Second extends Activity {
 	private TextView text;
@@ -17,7 +15,12 @@ public class Second extends Activity {
 	private Integer rest_timer;
 	private Integer interval;
 	public Integer n;
+	public long s1;
+	public long s2;
+	public String stage;
+
 	private static final String TAG = "MyActivity";
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,9 +39,9 @@ public class Second extends Activity {
 		text = (TextView) findViewById(R.id.textView1);
 
 		worktime = Integer.parseInt(work_timer) * 1000;
-		
+
 		n = 1;
-		
+
 		this.startWorkTimer(worktime);
 
 		// // Create the text view
@@ -53,56 +56,85 @@ public class Second extends Activity {
 
 	public void startWorkTimer(Integer length) {
 
-			 new CountDownTimer(length, 1000) {
+		new CountDownTimer(length, 1000) {
 
-				public void onTick(long millisUntilFinished) {
+			public void onTick(long millisUntilFinished) {
+				stage = "work";
+				s1 = millisUntilFinished;
 
-					text.setText("Work 00: " + millisUntilFinished / 1000);
-				}
+				text.setText(formatTime(millisUntilFinished));
+			}
 
-				public void onFinish() {
+			public void onFinish() {
 
-					new CountDownTimer(rest_timer, 1000) {
+				new CountDownTimer(rest_timer, 1000) {
 
-						public void onTick(long millisUntilFinished) {
-							text.setText("Rest 00: " + millisUntilFinished/ 1000);
+					public void onTick(long millisUntilFinished) {
+
+						stage = "rest";
+						s2 = millisUntilFinished;
+
+						text.setText(formatTime(millisUntilFinished));
+					}
+
+					public void onFinish() {
+
+						n++;
+						// Log.v(TAG, "meesage" + n);
+						if (n <= interval) {
+
+							startWorkTimer(worktime);
+
+						} else {
+							text.setText("done!");
 						}
+					}
 
-						public void onFinish() {
-							
-							n++;
-							//Log.v(TAG, "meesage" + n);
-							
-							if( n <= interval ){
-								startWorkTimer(worktime);
-							}else{
-								text.setText("done!");
-							}
-						}
+				}.start();
 
-					}.start();
+			}
 
-				}
+		}.start();
 
-			}.start();
-			
+	}
+
+	public void btnClick(View view) {
+
+		Button btn = (Button) findViewById(R.id.timeraction);
+
 		
+		
+	}
+
+	public String formatTime(long millis) {
+		String output = "00:00:00";
+		long seconds = millis / 1000;
+		long minutes = seconds / 60;
+		long hours = minutes / 60;
+
+		seconds = seconds % 60;
+		minutes = minutes % 60;
+		hours = hours % 60;
+
+		String secondsD = String.valueOf(seconds);
+		String minutesD = String.valueOf(minutes);
+		String hoursD = String.valueOf(hours);
+
+		if (seconds < 10)
+			secondsD = "0" + seconds;
+		if (minutes < 10)
+			minutesD = "0" + minutes;
+		if (hours < 10)
+			hoursD = "0" + hours;
+
+		output = hoursD + " : " + minutesD + " : " + secondsD;
+		return output;
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_second, menu);
 		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 
 }
